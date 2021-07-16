2cc76f014ad32f8b59e0d872b13a5c7f3cb7207b
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2021-07-16 01:00:11 -0400

    Updated text on home page...

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,44 +2,15 @@ import React from "react";
 
 export const Home = () => (
   <div>
-    <h1>Home:</h1>
+    <h1>Welcome...</h1>
     <p>
-      Should everyone with a criminal record really be serving a life sentence?
-      Does the punishment ever end? Does it discourage crime and make us safer,
-      or does it perpetuate our staggering recidivism rates? When will we end
-      this madness and what can we do to be a part of the solution? People
-      deserve a chance to change their lives and become active participants in
-      our society. By denying them these opportunities, we are discouraging
-      change and limiting their opportunity to pursue a better life.
-    </p>
-    <p>
-      Although crime victimization rates have been decreasing in the United
-      States, government spending on policing continues to increase.
-      Simultaneously our prison population continues to increase, destroying the
-      lives of countless Americans, who after serving lengthy jail or prison
-      sentences, completing probation or parole, and paying hefty fines and
-      restitution, are still increasingly finding themselves pushed out of their
-      communities. Many employers and landlords have strict policies which
-      prevent former offenders from being able to secure employment or housing.
-    </p>
-    <p>
-      We have all heard the shocking statistic: while only having 5% of the
-      world’s overall population, the US has 25% of the world’s prison
-      population. What fewer people realize is that although crime victimization
-      rates have been decreasing, government spending on policing, at the
-      federal, state, and local level, continue increasing. The path we have
-      taken as a nation has been a failure. Many of us accept this fact and push
-      it to the back of our conscience, hoping that someday, when the conditions
-      are right, we will work towards criminal justice reform.
-    </p>
-    <p>
-      The time for action is now. With renewed support for criminal justice
-      reform, and during a time of national reflection around the issues of
-      policing and crime, we are presented with an opportunity to re-envision a
-      rational and functional response to crime, one in which justice and
-      rehabilitation are centered, and where the goal is freedom, for our
-      communities, and those that have worked hard but have long had it denied
-      to them.
+      Thank you for visiting my website! I am currently a Computer Science
+      student with an Associate of Applied Sciences in Computer Information
+      Systems from Henry Ford College continuing my education in pursuit of a
+      Bachelors of Science in Computer Science. I created this site not only to
+      showcase the projects I have been a part of, but as a way to connect with
+      people. My goal is to continue pursuing my dreams and have some
+      interesting experiences along the way.
     </p>
   </div>
 );
