e8f10f718294d07b615e9d9539fb73e115fbd6bc
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2021-07-16 18:20:43 -0400

    Fixed footer position and styling, cleaned up comments, updated 404 error message...

--- src/NoMatch.jsx
+++ src/NoMatch.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export const NoMatch = () => (
   <div>
-    <h2>No Match</h2>
+    <h2 style={{ color: "#159cba" }}>
+      404 Error! We could not find what you were looking for.
+    </h2>
   </div>
 );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
-// import "../src/App.css";
 
 const Styles = styled.div`
   .navbar {
     background-color: #535353;
-    // position: sticky;
-    // flex-direction: column;
-    // bottom: 0;
-   
   }
 
   a,
@@ -26,42 +21,42 @@ const Styles = styled.div`
 `;
 
 export const Footer = () => (
-  <div id="dansFooter" style={{marginTop: "auto"}}>
-  <Styles>
-    <Navbar expand="lg" >
-      <Navbar.Brand href="/">DAJ</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/about">About</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/about">Projects</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/about">Blog</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link>
-              <Link to="/contact">Contact</Link>
-            </Nav.Link>
-          </Nav.Item>
-          <p class="text-white">&copy; Daniel Atoche-Juarez</p>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </Styles>
+  <div className="footer">
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">DAJ</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/">Home</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/about">About</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/about">Projects</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/about">Blog</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link>
+                <Link to="/contact">Contact</Link>
+              </Nav.Link>
+            </Nav.Item>
+            <p class="text-white">&copy; Daniel Atoche-Juarez</p>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
   </div>
 );
