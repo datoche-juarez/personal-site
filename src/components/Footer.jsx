ab6852502c66cb8f0405593d91d11fd388cb140d
dtoch <datoche-juarez@hawkmail.hfcc.edu>
2021-07-19 00:52:36 -0400

    A few styling touch ups...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router-dom";
 import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
 
+
 const Styles = styled.div`
   .navbar {
     background-color: #535353;
+    // position: sticky;
+    // flex-direction: column;
+    // bottom: 0;
+   
   }
 
   a,
@@ -21,42 +26,42 @@ const Styles = styled.div`
 `;
 
 export const Footer = () => (
-  <div className="footer">
-    <Styles>
-      <Navbar expand="lg">
-        <Navbar.Brand href="/">DAJ</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/">HOME</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/about">ABOUT</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/about">PROJECTS</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/about">BLOG</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>
-                <Link to="/contact">CONTACT</Link>
-              </Nav.Link>
-            </Nav.Item>
-            <p class="text-white">&copy; Daniel Atoche-Juarez</p>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    </Styles>
+  <div id="dansFooter" style={{marginTop: "auto"}}>
+  <Styles>
+    <Navbar expand="lg" >
+      <Navbar.Brand href="/">DAJ</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/">Home</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/about">About</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/about">Projects</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/about">Blog</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link>
+              <Link to="/contact">Contact</Link>
+            </Nav.Link>
+          </Nav.Item>
+          <p class="text-white">&copy; Daniel Atoche-Juarez</p>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
   </div>
 );
