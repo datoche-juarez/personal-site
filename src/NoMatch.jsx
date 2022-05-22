e923489be656fd74cc383405aa679fc944b6148d
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-05-21 21:57:04 -0400

    Fixed 404 ugly font color

--- src/NoMatch.jsx
+++ src/NoMatch.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const NoMatch = () => (
   <div>
-    <h2 style={{ color: "#159cba" }}>
+    <h2 >
       404 Error! We could not find what you were looking for.
     </h2>
   </div>
