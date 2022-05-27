bd31502ae28cac49b6a72947a35b5708ae066739
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-05-27 17:26:14 -0400

    Added project card screenshots

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./Projects.css";
 import { Button, Container, Row, Col, Card, CardGroup } from "react-bootstrap";
-
-
+import RAAScreenshot from "./assets/RAAScreenshot.png";
+import PingScreenshot from "./assets/PingScreenshot.png";
+import CordialScreenshot from "./assets/CSScreenshot.png";
 
 export const Projects = () => (
   <div>
@@ -19,49 +20,50 @@ export const Projects = () => (
     >
       Github
     </Button>
-    <Container style={{paddingTop: '20px', paddingBottom: '30px'}}>
-    <CardGroup>
-  <Card>
-    <Card.Img variant="top" src="assets/RAA-Screenshot.png" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
+    <Container style={{ paddingTop: "20px", paddingBottom: "30px" }}>
+      <CardGroup>
+        <Card>
+          <Card.Img variant="top" src={RAAScreenshot} style={{ height: '45%'}} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={PingScreenshot} style={{ height: '45%'}} />
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This card has supporting text below as a natural lead-in to
+              additional content.{" "}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+        <Card>
+          <Card.Img variant="top" src={CordialScreenshot} style={{ height: '45%' }}/>
+          <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </Card.Footer>
+        </Card>
+      </CardGroup>
     </Container>
   </div>
 );
