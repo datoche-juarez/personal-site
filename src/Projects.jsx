4ac8105a3ee47aab81ffa0f1523526848213972c
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-05-27 16:21:49 -0400

    Adding photos to assets..

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Projects.css";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row, Col, Card, CardGroup } from "react-bootstrap";
+
+
 
 export const Projects = () => (
   <div>
@@ -17,5 +19,49 @@ export const Projects = () => (
     >
       Github
     </Button>
+    <Container style={{paddingTop: '20px', paddingBottom: '30px'}}>
+    <CardGroup>
+  <Card>
+    <Card.Img variant="top" src="assets/RAA-Screenshot.png" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardGroup>
+    </Container>
   </div>
 );
