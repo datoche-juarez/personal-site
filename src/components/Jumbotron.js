4751d9ae2e994d286ee6a4e596518d7fd1eed58a
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-05-27 21:14:51 -0400

    Updated banner and styling

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 import styled from "styled-components";
-import DAJBanner from "../assets/DAJBanner.jpg";
+// import DAJBanner from "../assets/DAJBanner.jpg";
+import DAJBanner from "../assets/Banner.png";
 
 const Styles = styled.div`
   // .jumbo {
@@ -21,16 +22,16 @@ const Styles = styled.div`
     z-index: -2;
   }
 
-  // .overlay {
-  //   background-color: #000;
-  //   opacity: 0.2;
-  //   position: absolute;
-  //   top: 0;
-  //   left: 0;
-  //   bottom: 0;
-  //   right: 0;
-  //   z-index: -1;
-  // }
+  .overlay {
+    background-color: #ff7733;
+    opacity: 0.13;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+  }
 `;
 
 export const Jumbotron = () => (
