6f85768903eca7198d2f4038675b3faaccddf948
Daniel Atoche-Juarez <datoche-juarez@Daniels-MacBook-Pro.local>
2022-05-30 02:17:18 -0400

    Brand logo css updates

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,7 +4,7 @@ import { Nav, Navbar } from "react-bootstrap";
 import styled from "styled-components";
 
 import { Container } from "react-bootstrap";
-import DAJLogo from '../assets/DajLogo3.png';
+import DAJLogo from "../assets/DajLogo3.png";
 
 const Styles = styled.div`
   .navbar {
@@ -17,7 +17,7 @@ const Styles = styled.div`
   .navbar-nav,
   .nav-link {
     color: #ffffff;
-    padding-top: 15px;;
+    padding-top: 15px;
     text-align: center;
 
     &:hover {
@@ -28,19 +28,30 @@ const Styles = styled.div`
     float: left;
     margin: 5px;
   }
+
+  .navbar-brand:hover {
+    margin-bottom: 5px;
+    .frame {
+      box-shadow: 3px 5px 10px gray !important;
+    }
+  }
+
   .container {
     margin: auto;
   }
-
 `;
 
 export const NavigationBar = () => (
   <Styles>
-    <Navbar expand="lg" variant="dark" >
-    <Container>
+    <Navbar expand="lg" variant="dark">
+      <Container>
         <div className="w-100 small text-right align-self-end">
-        <Navbar.Brand href="/"><img src={DAJLogo} weign="40" height="60"/></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"  />
+          <Navbar.Brand href="/">
+            <div className="frame">
+              <img src={DAJLogo} weign="40" height="60" />
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Item>
